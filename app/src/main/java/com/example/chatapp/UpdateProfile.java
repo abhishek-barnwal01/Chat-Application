bd6d889b4709c35ce74259e010286947bc153c83
abhishek <aabarnwal01@gmail.com>
2022-07-09 23:28:42 +0530

    xml editing

--- app/src/main/java/com/example/chatapp/UpdateProfile.java
+++ app/src/main/java/com/example/chatapp/UpdateProfile.java
@@ -1,14 +1,73 @@
 package com.example.chatapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class UpdateProfile extends AppCompatActivity {
+
+    private  EditText mnewusername;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+
+
+    private FirebaseFirestore firebaseFirestore;
+
+    private  ImageView mgetnewimageinimageview;
+
+    private StorageReference storageReference;
+
+    private String ImageURIacessToken;
+
+    private androidx.appcompat.widget.Toolbar mtoolbarofupdateprofile;
+    private ImageButton mbackbuttonofupdateprofile;
+
+
+    private FirebaseStorage firebaseStorage;
+
+
+    ProgressBar mprogressbarofupdateprofile;
+
+    private Uri imagepath;
+
+    Intent intent;
+
+    private static int PICK_IMAGE=123;
+
+    android.widget.Button mupdateprofilebutton;
+    String newname;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+
+        mtoolbarofupdateprofile=findViewById(R.id.toolbarofupdateprofile);
+        mbackbuttonofupdateprofile=findViewById(R.id.backbuttonofupdateprofile);
+        mgetnewimageinimageview=findViewById(R.id.getnewuserimageinimageview);
+        mprogressbarofupdateprofile=findViewById(R.id.progressbarofupdateprofile);
+        mnewusername=findViewById(R.id.getnewusername);
+        mupdateprofilebutton=findViewById(R.id.updateprofilebutton);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        firebaseStorage=FirebaseStorage.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+
+        intent=getIntent();
+
     }
 }
